cc566aa1bfb2feba2a1250580f57841a632dd2fb
Shadowlife2 <86638135+Shadowlife2@users.noreply.github.com>
2021-10-11 12:27:41 -0400

    Add files via upload

---
+++ tabz2.js
@@ -0,0 +1,7 @@
+$('.tab').on('click', function(evt) {
+  evt.preventDefault();
+  $(this).toggleClass('active');
+  var sel = this.getAttribute('data-toggle-target');
+  $('.tab-content').removeClass('active').filter(sel).addClass('active');
+});
+    
